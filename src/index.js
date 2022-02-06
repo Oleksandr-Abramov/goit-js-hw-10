62e08e78f69309222e70d0063d74772fe2def7e1
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-02-06 23:31:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,42 +4,65 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-
+let markup = '';
 const refs = {
   input: document.querySelector('#search-box'),
   ulItems: document.querySelector('.country-list'),
 };
 
-refs.input.addEventListener(
-  'input',
-  debounce(e => {
-    const inputValue = e.target.value.trim();
-    fetchCountries(inputValue)
-      .then(countries => renderCountriesList(countries))
-      .catch(error => {
-        refs.ulItems.innerHTML = '';
-        Notify.failure(`❌ ${error} Oops, there is no country with that name`);
-      });
-  }, DEBOUNCE_DELAY),
-);
+refs.input.addEventListener('input', debounce(postInputApi, DEBOUNCE_DELAY));
 
-function renderCountriesList(countries) {
-  if (countries.length > 10) {
-    Notify.success(`Too many matches found. Please enter a more specific name`);
+function postInputApi() {
+  const inputValue = refs.input.value.trim();
+
+  if (inputValue === '') {
     return;
   }
 
-  const markup = countries
-    .map(({ capital, flags: { svg }, languages, name: { official }, population }) => {
-      if (countries.length === 1) {
-        return `<li class="li"><div class="country"><img class="icon" src="${svg}" width="40px"><H3>${official}</H3></div><p><b>Capital:</b> ${capital}</p><p><b>Population:</b> ${population}</p><p><b>language:</b> ${Object.values(
-          languages,
-        )}</p></li>`;
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        return toMachCoutries(countries);
       }
-
-      return `<li class="country"><img class="icon" src="${svg}" width="40px"><H4>${official}</H4></li>`;
+      if (countries.length === 1) {
+        return oneCountry(countries);
+      }
+      listCountries(countries);
+      // renderCountriesList(countries)
     })
-    .join('');
+    .catch(error => {
+      refs.ulItems.innerHTML = '';
+      Notify.failure(`❌ ${error} Oops, there is no country with that name`);
+    });
+}
 
+function toMachCoutries() {
+  Notify.success(`Too many matches found. Please enter a more specific name`);
+  return;
+}
+
+function oneCountry(countries) {
+  markup = countries
+    .map(
+      ({ capital, flags: { svg }, languages, name: { official }, population }) =>
+        `<li class="li"><div class="country"><img class="icon" src="${svg}" width="40px"><H3>${official}</H3></div><p><b>Capital:</b> ${capital}</p><p><b>Population:</b> ${population}</p><p><b>language:</b> ${Object.values(
+          languages,
+        )}</p></li>`,
+    )
+    .join('');
+  renderCountreisList();
+}
+
+function listCountries(countries) {
+  markup = countries
+    .map(
+      ({ flags: { svg }, name: { official } }) =>
+        `<li class="country"><img class="icon" src="${svg}" width="40px"><H4>${official}</H4></li>`,
+    )
+    .join('');
+  renderCountreisList();
+}
+
+function renderCountreisList() {
   refs.ulItems.innerHTML = markup;
 }
