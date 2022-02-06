95c949c009f8be58e9da4dcce126bc74d27899d3
Oleksandr Abramov <aleksander.abramov@gmail.com>
2022-02-06 16:29:50 +0200

    in process

--- src/index.js
+++ src/index.js
@@ -29,11 +29,11 @@ function renderCountriesList(countries) {
       if (countries.length === 1) {
         console.log(capital, svg, languages, official, population);
         // return `<li><svg class="flag" width="120px"><use href="${svg}"></use></svg><p>${official}</p></li>`;
-        return `<li class="country"><img class="icon" src="${svg}" width="40px"><H3>${official}</H3><p><b>Capital:</b> ${capital}</p><p><b>Population:</b> ${population}</p><p><b>language:</b> ${Object.values(
+        return `<li class="li"><div class="country"><img class="icon" src="${svg}" width="40px"><H3>${official}</H3></div><p><b>Capital:</b> ${capital}</p><p><b>Population:</b> ${population}</p><p><b>language:</b> ${Object.values(
           languages,
         )}</p></li>`;
       }
-      return `<li><svg class="flag" width="20px" height="20px"><use href="${svg}"></use></svg>${official}</li>`;
+      return `<li class="country"><svg class="flag" width="20px" height="20px"><use href="${svg}"></use></svg>${official}</li>`;
     })
     .join('');
   refs.ulItems.innerHTML = markup;
